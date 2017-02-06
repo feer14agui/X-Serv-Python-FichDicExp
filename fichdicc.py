f4acac6676fe6fd6b24d4217595725ec98e03beb
feer14agui <f14agui@gmail.com>
2017-02-06 13:42:23 +0100

    ejercicio hecho

---
+++ fichdicc.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#Fernando Aguilar Santos
+
+fd = open('/etc/passwd', 'r')
+
+Dicc = {}							#Declaro el diccionario
+lineas = fd.readlines()			
+fd.close()
+
+for linea in lineas:
+	elementos = linea.split(':')
+	Dicc[elementos[0]]= elementos[-1][:-1] 			#Guardo cada key y value en el diccionario(1 a 1)
+
+try:
+
+	print (Dicc["root"])
+	print (Dicc["imaginario"])
+
+except :			#elevo la excepción para el caso de que no encuentro la key
+
+	print ("key no encontrada")
